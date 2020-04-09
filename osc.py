Fixed math errors regarding Jacobians

osc.py
@@ -54,6 +54,7 @@ class Control(control.Control):
         # calculate the Jacobian
         JEE = robot.gen_jacEE()
 
+
         # calculate desired end-effector acceleration
         if x_dd_des is None:
             # self.pid.setpoint = self.target
@@ -75,9 +76,9 @@ class Control(control.Control):
         tau_grav = robot.gen_grav()
 
         # add in velocity compensation in GC space for stability
-        # self.u = (np.dot(JEE.T, Fx).reshape(-1, )) # + tau_grav
-        self.u = (np.dot(JEE.T, Fx).reshape(-1, ) -
-                  np.dot(Mq, self.kd * robot.dq).flatten()) + tau_grav
+        self.u = (np.dot(JEE.T, Fx).reshape(-1, )) - tau_grav
+        # self.u = (np.dot(JEE.T, Fx).reshape(-1, ) -
+        #          np.dot(Mq, self.kd * robot.dq).flatten()) + tau_grav
 
         # simple inverse kinematics PID control
         # self.u = self.kp*(robot.inv_kinematics(self.target)-robot.q)
@@ -126,5 +127,6 @@ class Control(control.Control):
         # self.target = np.random.random(size=(3,)) * gain + bias
 
         self.target = np.array([0.077, 0.131, -0.708])
+        # self.target = np.array([0, 0, -0.8325])
 
         return self.target.tolist()

robot.py
@@ -48,6 +48,7 @@ class Robot(RobotBase):
         comz = values[3].astype(np.float)
 
         mass = values[7].astype(np.float)
+
         ixx = values[8].astype(np.float)
         ixy = values[9].astype(np.float)
         ixz = values[10].astype(np.float)
@@ -107,7 +108,6 @@ class Robot(RobotBase):
             M[5, 3] = -ixz[i]
             M[5, 4] = -iyz[i]
             M[5, 5] = izz[i]
-            # self.MM.insert(i,M)
             self.MM.append(M)
             fgi[2,0] = float(mass[i])*(-9.807) # apply mass*gravity term to 3rd column (z axis)
             self.Fg.append(fgi)
@@ -140,7 +140,7 @@ class Robot(RobotBase):
         JCOM1 = np.zeros((6, 4))
         JCOM1[1, 0] = -L1 * np.sin(q1) - 2 * coml1 * np.sin(2 * q1)
         JCOM1[2, 0] = L1 * np.cos(q1) + 2 * coml1 * np.cos(2 * q1)
-        JCOM1[3, :] = 1
+        JCOM1[3, 0] = 1
 
         return JCOM1
 
@@ -162,7 +162,8 @@ class Robot(RobotBase):
         JCOM2[1, 1] = -L2 * np.sin(q2) * np.cos(q1)
         JCOM2[2, 0] = (L1 + L2 * np.cos(q2) + coml2) * np.cos(q1)
         JCOM2[2, 1] = -L2 * np.sin(q1) * np.sin(q2)
-        JCOM2[5, :] = 1
+        JCOM2[3, 0] = 1
+        JCOM2[5, 1] = 1
 
         return JCOM2
 
@@ -201,7 +202,9 @@ class Robot(RobotBase):
             + coml3 * np.sin(q2 + q3) * np.cos(q3)
         JCOM3[2, 0] = (L1 + coml3 * np.cos(q3)) * np.cos(q1)
         JCOM3[2, 2] = -coml3 * np.sin(q1) * np.sin(q3)
-        JCOM3[5, :] = 1
+        JCOM3[3, 0] = 1
+        JCOM3[5, 1] = 1
+        JCOM3[5, 2] = 1
 
         return JCOM3
 
@@ -252,7 +255,10 @@ class Robot(RobotBase):
             + coml4 * np.sin(q2 + q3 + q4) * np.cos(q4)
         JCOM4[2, 0] = (L1 + coml4 * np.cos(q4)) * np.cos(q1)
         JCOM4[2, 3] = -coml4 * np.sin(q1) * np.sin(q4)
-        JCOM4[5, :] = 1
+        JCOM4[3, 0] = 1
+        JCOM4[5, 1] = 1
+        JCOM4[5, 2] = 1
+        JCOM4[5, 3] = 1
 
         return JCOM4
 
@@ -282,6 +288,10 @@ class Robot(RobotBase):
                           - np.sin(-q1 + q2 + q3 + 2 * q4) / 2 - np.sin(q1 + q2 + q3 + 2 * q4) / 2)
         JEE[2, 0] = L4 * np.cos(q1) * np.cos(q4)
         JEE[2, 3] = -L4 * np.sin(q1) * np.sin(q4)
+        # JEE[3, 0] = 1
+        # JEE[5, 1] = 1
+        # JEE[5, 2] = 1
+        # JEE[5, 3] = 1
 
         return JEE
 
@@ -295,7 +305,7 @@ class Robot(RobotBase):
         JCOM1 = self.gen_jacCOM1(q=q)
         JCOM2 = self.gen_jacCOM2(q=q)
         JCOM3 = self.gen_jacCOM3(q=q)
-        JCOM4 = self.gen_jacCOM3(q=q)
+        JCOM4 = self.gen_jacCOM4(q=q)
 
         Mq = (np.dot(JCOM1.T, np.dot(M1, JCOM1)) +
               np.dot(JCOM2.T, np.dot(M2, JCOM2)) +