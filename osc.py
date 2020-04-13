Fixed end effector jacobian

osc.py
@@ -50,7 +50,7 @@ class Control(control.Control):
         x_dd_des np.array: desired acceleration
         """
         # which dim to control of [x, y, z, alpha, beta, gamma]
-        ctrlr_dof = np.array([True, True, True, False, True, False])
+        ctrlr_dof = np.array([True, True, True, False, False, False])
 
         # calculate the Jacobian
         JEE = robot.gen_jacEE()[ctrlr_dof]
@@ -137,6 +137,6 @@ class Control(control.Control):
         target_alpha = 0
         target_beta = np.pi # keep foot flat for now
         target_gamma = 0
-        self.target = np.array([0.077, 0.131, -0.608, target_alpha, target_beta, target_gamma])
+        self.target = np.array([0.077, 0.131, -0.708, target_alpha, target_beta, target_gamma])
 
         return self.target.tolist()

robot.py
@@ -272,23 +272,34 @@ class Robot(RobotBase):
         q3 = q[2]
         q4 = q[3]
 
+        L1 = self.L[1]
+        L2 = self.L[2]
+        L3 = self.L[3]
         L4 = self.L[4]
 
         JEE = np.zeros((6, 4))  # (3, 4) if only x, y, z forces controlled, others dropped
-        JEE[0, 0] = L4 * np.sin(q1) * np.sin(q2 + q3 + q4) * np.cos(q4)
-        JEE[0, 1] = -L4 * (np.sin(q4) * np.sin(q2 + q3 + q4)
-                           + np.cos(q1) * np.cos(q4) * np.cos(q2 + q3 + q4))
-        JEE[0, 2] = JEE[0, 1]
-        JEE[0, 3] = L4 * (np.cos(q2 + q3 + 2 * q4)
-                          - np.cos(-q1 + q2 + q3 + 2 * q4) / 2 - np.cos(q1 + q2 + q3 + 2 * q4) / 2)
-        JEE[1, 0] = -L4 * np.sin(q1) * np.cos(q4) * np.cos(q2 + q3 + q4)
-        JEE[1, 1] = L4 * (np.sin(q4) * np.cos(q2 + q3 + q4)
-                          - np.sin(q2 + q3 + q4) * np.cos(q1) * np.cos(q4))
-        JEE[1, 2] = JEE[1, 1]
-        JEE[1, 3] = L4 * (np.sin(q2 + q3 + 2 * q4)
-                          - np.sin(-q1 + q2 + q3 + 2 * q4) / 2 - np.sin(q1 + q2 + q3 + 2 * q4) / 2)
-        JEE[2, 0] = L4 * np.cos(q1) * np.cos(q4)
-        JEE[2, 3] = -L4 * np.sin(q1) * np.sin(q4)
+        JEE[0, 0] = (L1 + L4*np.cos(q4))*np.sin(q1)*np.sin(q2 + q3 + q4)
+        JEE[0, 1] = -L1*np.cos(q1)*np.cos(q2 + q3 + q4) + L2*np.cos(-q2 + q3 + q4)\
+                    - L4*np.sin(q4)*np.sin(q2 + q3 + q4)\
+                    - L4*np.cos(q1)*np.cos(q4)*np.cos(q2 + q3 + q4)
+        JEE[0, 2] = -L1*np.cos(q1)*np.cos(q2 + q3 + q4) - L2*np.cos(-q2 + q3 + q4) + L3*np.cos(q3 - q4)\
+                    - L4*np.sin(q4)*np.sin(q2 + q3 + q4) - L4*np.cos(q1)*np.cos(q4)*np.cos(q2 + q3 + q4)
+        JEE[0, 3] = -L1*np.cos(q1)*np.cos(q2 + q3 + q4) - L2*np.cos(-q2 + q3 + q4) - L3*np.cos(q3 - q4)\
+                    + L4*np.sin(q4)*np.sin(q2 + q3 + q4)*np.cos(q1) - L4*np.sin(q4)*np.sin(q2 + q3 + q4)\
+                    - L4*np.cos(q1)*np.cos(q4)*np.cos(q2 + q3 + q4)\
+                    + L4*np.cos(q4)*np.cos(q2 + q3 + q4) + L4*np.cos(q4)
+        JEE[1, 0] = -(L1 + L4*np.cos(q4))*np.sin(q1)*np.cos(q2 + q3 + q4)
+        JEE[1, 1] = -L1*np.sin(q2 + q3 + q4)*np.cos(q1) + L2*np.sin(-q2 + q3 + q4)\
+                    + L4*np.sin(q4)*np.cos(q2 + q3 + q4)\
+                    - L4*np.sin(q2 + q3 + q4)*np.cos(q1)*np.cos(q4)
+        JEE[1, 2] = -L1*np.sin(q2 + q3 + q4)*np.cos(q1) - L2*np.sin(-q2 + q3 + q4) - L3*np.sin(q3 - q4)\
+                    + L4*np.sin(q4)*np.cos(q2 + q3 + q4) - L4*np.sin(q2 + q3 + q4)*np.cos(q1)*np.cos(q4)
+        JEE[1, 3] = -L1*np.sin(q2 + q3 + q4)*np.cos(q1) - L2*np.sin(-q2 + q3 + q4) + L3*np.sin(q3 - q4)\
+                    - L4*np.sin(q4)*np.cos(q1)*np.cos(q2 + q3 + q4) + L4*np.sin(q4)*np.cos(q2 + q3 + q4)\
+                    - L4*np.sin(q4) - L4*np.sin(q2 + q3 + q4)*np.cos(q1)*np.cos(q4)\
+                    + L4*np.sin(q2 + q3 + q4)*np.cos(q4)
+        JEE[2, 0] = (L1 + L4*np.cos(q4))*np.cos(q1)
+        JEE[2, 3] = -L4*np.sin(q1)*np.sin(q4)
         JEE[3, 0] = 1
         JEE[5, 1] = 1
         JEE[5, 2] = 1